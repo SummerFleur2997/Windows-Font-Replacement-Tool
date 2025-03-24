8fed8822f0a79c4bad82740199c0644aa81657d1
SummerFleur <summerfleur@qq.com>
2025-03-24 14:05:40 +0800

    new: add csharp codes

---
+++ CSharpCode/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Windows_Font_Replacement_Tool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // DetectSystemIconFont();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void Border_MouseDown(object sender, RoutedEventArgs e)
+        {
+            DragMove();
+        }
+        
+        private void TabButton_Click(object sender, RoutedEventArgs e)
+        {
+            Tab1Content.Visibility = Visibility.Collapsed;
+            Tab2Content.Visibility = Visibility.Collapsed;
+            Tab3Content.Visibility = Visibility.Collapsed;
+            
+            var btn = sender as Button;
+            switch (btn?.Name)
+            {
+                case "Tab1Btn":
+                    Tab1Content.Visibility = Visibility.Visible;
+                    break;
+                case "Tab2Btn":
+                    Tab2Content.Visibility = Visibility.Visible;
+                    break;
+                case "Tab3Btn":
+                    Tab3Content.Visibility = Visibility.Visible;
+                    break;
+            }
+            
+            foreach (var child in ((StackPanel)btn.Parent).Children)
+            {
+                if (child is Button b)
+                {
+                    b.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#6750A4"));
+                }
+            }
+            btn.Background = new SolidColorBrush(Colors.DarkViolet);
+        }
+    }
+}
--- PythonCodes/functions.py
+++ PythonCodes/functions.py
@@ -1,46 +1,40 @@
 import os
+import sys
 import argparse
 from fontTools.ttLib import TTFont
 from fontTools.ttLib.ttCollection import TTCollection
-
-# selected_font = []
-#
-# for _ in range(16):
-#     selected_font.append(None)
 
 # 定义资源文件路径
 path = os.getcwd()
 
 
-# def OpenHelpDoc():
-#     os.startfile(f"{path}\\Libs\\Help.pdf")
-#
-#
-# def OpenOutputDir():
-#     os.startfile(f"{path}\\output")
-#
-#
-# def InitOutput():
-#
-#     time = str(datetime.datetime.now())[0:19]
-#     dirname = time.replace('-', '').replace(':', '').replace(' ', '_')
-#     dirname = dirname + "_" + os.path.basename(selected_font[0])[0:-4]
-#
-#     os.makedirs(f"{path}\\output\\{dirname}")
-#
-#     for file in os.listdir(f"{path}\\output\\cache"):
-#         if file[0] == 's' or file[0] == 'S':
-#             src = os.path.join(f"{path}\\output\\cache", file)
-#             dst = os.path.join(f"{path}\\output\\{dirname}", file)
-#             shutil.move(src, dst)
-#
-#     return dirname
+def _getFontFamily(ttfont: TTFont) -> str:
+    """内部函数\n
+    输入一个 ttfont 字体，返回其 FontFamily 属性。
+    :param ttfont: ttf 字体
+    :return: ttf 字体的 FontFamily 属性
+    """
+    name_table = ttfont["name"]
+    font_family = ""
+    font_subfamily = ""
+    for data in name_table.names:
+        if data.platformID != 3:
+            continue
+        if data.nameID == 1 and data.langID == 0x409:
+            font_family = data.toUnicode()
+        if data.nameID == 2 and data.langID == 0x409:
+            font_subfamily = data.toUnicode()
+
+    return "" + font_family + "-" + font_subfamily
 
 
 def fontPropertyReplace(args) -> int:
     """
-    将 args.xml 中的 name 表数据转移至 args.font 中，进行字体属性替换。
-    :param args: 命令行传递参数
+    将 args.xml 中的 name 表数据转移至 args.font 中，进行字体属性替换。命令行使用样例：
+
+    functions.exe propertyRep [font绝对路径] [xml绝对路径] --path [带有目标文件名的目录名称]
+
+    将会默认保存至 cache 目录，若提供可选参数 --path，则保存为 path
     """
     if args.font is None:
         return 1001
@@ -49,15 +43,22 @@ def fontPropertyReplace(args) -> int:
     temp_font = TTFont(args.xml)
     targeted_font = TTFont(args.font)
     targeted_font["name"] = temp_font["name"]
-    targeted_font.save(f"{path}\\output\\cache\\{os.path.basename(args.xml)}")
+    if not args.path:
+        targeted_font.save(f"{path}\\output\\cache\\{os.path.basename(args.xml)}")
+    else:
+        targeted_font.save(f"{path}\\output\\{args.path}")
 
     return 0
 
 
 def mergeTTC(args) -> int:
     """
-    将指定的 ttf 合并为 ttc，并保存至 args.dirname 文件夹内。
-    :param args: 命令行传递参数
+    将指定的 ttf 合并为 ttc，并保存至 args.dirname 文件夹内。命令行使用样例：
+
+    functions.exe mergeTTC [dirname目录名称] [ttc字体集文件名]
+
+    将会把 cache 目录下名为 [ttc]01.ttf, [ttc]02.ttf ... 等等文件合并为一个 ttc 字体集，
+    然后存储到 [dirname] 目录下，名为 [ttc].ttc
     """
     TTC = TTCollection()
     num = 1
@@ -71,30 +72,48 @@ def mergeTTC(args) -> int:
     return 0
 
 
-def splitTTC(arg) -> int:
+def splitTTC(args) -> int:
     """
-    给定 arg.ttc 字体集文件，将其拆分为 ttf。
-    :param arg: 命令行传递参数
+    给定 arg.ttc 字体集文件，将其拆分为 ttf。命令行使用样例：
+
+    functions.exe splitTTC [ttc绝对路径] --path [目录名称]
     """
-    if arg is None:
+    if args is None:
         return 3001
     index = 1
-    name = os.path.basename(arg.ttc)[:-4]
-    TTC = TTCollection(arg.ttc)
-    for ttf in TTC.fonts:
-        ttf.save(f"{path}\\output\\cache\\{name}{index:02d}.ttf")
-        index += 1
+    name = os.path.basename(args.ttc)[:-4]
+    TTC = TTCollection(args.ttc)
+    if not args.path:
+        for ttf in TTC.fonts:
+            ttf.save(f"{path}\\output\\cache\\{name}{index:02d}.ttf")
+            index += 1
+    else:
+        for ttf in TTC.fonts:
+            fontFamily = _getFontFamily(ttf)
+            ttf.save(f"{path}\\output\\{args.path}\\{index:02d}_{fontFamily}.ttf")
+            index += 1
 
     return 0
 
 
+def getFontFamily(arg) -> str:
+    """
+    _getFontFamily() 函数的命令行调用形式。命令行使用样例：
+
+    functions.exe fontFamily [ttf绝对路径]
+    """
+    ttfont = TTFont(arg.ttf)
+    return _getFontFamily(ttfont)
+
+
 def main():
-    parser = argparse.ArgumentParser(description="我的脚本")
-    subparsers = parser.add_subparsers(help="子命令帮助")
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
 
     parser_func1 = subparsers.add_parser("propertyRep")
     parser_func1.add_argument("font")           # 个性化字体文件，应为绝对路径
     parser_func1.add_argument("xml")            # 带有 msyh 原始数据的文件，应为绝对路径
+    parser_func1.add_argument("--path", required=False, default=False, type=str)
     parser_func1.set_defaults(func=fontPropertyReplace)
 
     parser_func2 = subparsers.add_parser("mergeTTC")
@@ -104,28 +123,24 @@ def main():
 
     parser_func3 = subparsers.add_parser("splitTTC")
     parser_func3.add_argument("ttc")            # 需要拆分的 ttc 文件，应为绝对路径
+    parser_func3.add_argument("--path", required=False, default=False, type=str)
     parser_func3.set_defaults(func=splitTTC)
 
-    args = parser.parse_args()
-    args.func(args)
+    parser_func4 = subparsers.add_parser("fontFamily")
+    parser_func4.add_argument("ttf")
+    parser_func4.set_defaults(func=getFontFamily)
 
-# def getSHA1(name):
-#     with open(f"{path}/Libs/{name}", 'rb') as db:
-#         return hashlib.new("sha1", db.read()).hexdigest()
-#
-#
-# def DecodeLibs():
-#     with open(f"{path}/Libs/dataLibs.lib", 'rb') as f:
-#         data = f.read()
-#     with open(f"{path}/Libs/archive", 'wb') as f:
-#         archive = base64.b64decode(data)
-#         f.write(archive)
-#     with zipfile.ZipFile(f"{path}/Libs/archive", "r") as f:
-#         f.extractall(f'{path}/Libs/xmls', pwd=b"SaXNnYbgwMjDvgyl")
-#         f.close()
-#     sha1 = getSHA1("archive")
-#     os.remove(f"{path}/Libs/archive")
-#     return sha1
+    args = parser.parse_args()
+    try:
+        result = args.func(args)
+        if isinstance(result, str):
+            print(result)
+            sys.exit(0)
+        else:
+            sys.exit(result)
+    except Exception as e:
+        print(f"未捕获的异常: {str(e)}", file=sys.stderr)
+        sys.exit(99)
 
 
 if __name__ == "__main__":
